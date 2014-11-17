6346991455e6f0aaf1f333ae1d7896d7690461c8
larnacoeurx <larnacoeurx@gmail.com>
2014-11-17 17:42:46 +0100

    dzialajace lab5

--- lab5.cpp
+++ lab5.cpp
@@ -24,29 +24,23 @@ int min(int*& tab, const int& size1)
 	return min;
 }
 
-bool czyNalezy(const int& rank, int*& grupa, const int& rozmiar)
-{
-	if(rank == 0)
-		return false;
-	for(int i=0; i < rozmiar; i++)
-		if(grupa[i] == rank)
-		{
-			return true;
-		}
-	return false;
-}
-
 int main(int argc, char** argv)
 {
 	MPI_Init(&argc, &argv);
 	srand(time(NULL));
-	int size, rank, rankL, size1, size2, *tab, rozmiar, rozmiarCzesci, *tabL, maxL, maxG;
+	int size, rank, rankL, size1, size2, *tab, rozmiar, rozmiarCzesci, *tabL, maxL, maxG, minG;
 	int master, masterG1, masterG2, masters[3];
 	MPI_Group wszystkie, workG1, workG2, mastersG;
 	MPI_Comm COMM_workG1, COMM_workG2, COMM_mastersG;
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	
+	if(size < 3)
+	{
+		if(rank == 0)
+			cout << "Ilosc procesow to conajmniej 3" << endl;
+		MPI_Finalize();
+		return 0;
+	}
 	size1 = (int) ((size-1)/2);
 	int* pierwszaG = new int[size1];
 	size2 = (int) (size - (size1+1));
@@ -75,7 +69,7 @@ int main(int argc, char** argv)
 	masters[2] = size1+1;
 	MPI_Group_incl(wszystkie, 3, masters, &mastersG);
 	MPI_Comm_create(MPI_COMM_WORLD, mastersG, &COMM_mastersG);
-	if(rank == 0)
+	if(rank == masters[0])
 	{
 		rozmiar = size*size1*size2;
 		tab = new int[rozmiar];
@@ -89,33 +83,39 @@ int main(int argc, char** argv)
 			tab = new int[rozmiar];
 		MPI_Bcast(tab, rozmiar, MPI_INT, 0, COMM_mastersG);
 	}
-
-	if(rank <= size1 && rank != 0)
+	if(rank != masters[0])
 	{
-		rozmiarCzesci = rozmiar/size1;
-		int* lokalna = new int[rozmiarCzesci];
-		MPI_Comm_rank(COMM_workG1, &rankL);
-		MPI_Bcast(&rozmiarCzesci, 1, MPI_INT, 0, COMM_workG1);
-		cout << rankL << " " << rank << " rozmiar czesci: " << rozmiarCzesci << endl;
-		if(rankL == 0)
+		if(rank <= size1)
 		{
-			MPI_Send(tab, rozmiarCzesci, MPI_INT, 1, 0, COMM_workG1);
+			rozmiarCzesci = rozmiar/size1;
+			tabL = new int[rozmiarCzesci];
+			MPI_Comm_rank(COMM_workG1, &rankL);
+			MPI_Bcast(&rozmiarCzesci, 1, MPI_INT, 0, COMM_workG1);
+			MPI_Scatter(tab, rozmiarCzesci, MPI_INT, tabL, rozmiarCzesci, MPI_INT, 0, COMM_workG1);
+			maxL = max(tabL, rozmiarCzesci);
+			MPI_Reduce(&maxL, &maxG, 1, MPI_INT, MPI_MAX, 0, COMM_workG1);
+			if(rankL == 0)
+				MPI_Send(&maxG, 1, MPI_INT, 0, 0, COMM_mastersG);
 		}
-		else if(rankL == 1)
+		else if(rank > size1)
 		{
-			MPI_Recv(lokalna, rozmiarCzesci, MPI_INT, 1, 0, COMM_workG1, MPI_STATUS_IGNORE);
-			cout << lokalna[2] << endl;
-
-		}
-		// for(int i=1; i<rozmiarCzesci; i++)
-		// 	if(tab[i] > max)
-		// 		max = tab[i];
-		// cout << max << endl;
-		// MPI_Reduce(&maxL, &maxG, 1, MPI_INT, MPI_MAX, 0, COMM_workG1);
-		// if(rankL == 0)
-		// 	cout << maxG << endl;
+			rozmiarCzesci = rozmiar/size2;
+			tabL = new int[rozmiarCzesci];
+			MPI_Comm_rank(COMM_workG2, &rankL);
+			MPI_Bcast(&rozmiarCzesci, 1, MPI_INT, 0, COMM_workG2);
+			MPI_Scatter(tab, rozmiarCzesci, MPI_INT, tabL, rozmiarCzesci, MPI_INT, 0, COMM_workG2);
+			maxL = min(tabL, rozmiarCzesci);
+			MPI_Reduce(&maxL, &maxG, 1, MPI_INT, MPI_MIN, 0, COMM_workG2);
+			if(rankL == 0)
+				MPI_Send(&maxG, 1, MPI_INT, 0, 0, COMM_mastersG);
+		}	
 	}
-	// cout << masterG1 << masterG2 << "!!!!" << endl;
-	// MPI_Group_incl(wszystkie, 3, masters, &mastersG);
+	else
+	{
+		MPI_Recv(&maxG, 1, MPI_INT, 1, 0, COMM_mastersG, MPI_STATUS_IGNORE);
+		MPI_Recv(&minG, 1, MPI_INT, 2, 0, COMM_mastersG, MPI_STATUS_IGNORE);
+		cout << "MIN: " << minG << ", MAX: " << maxG << endl;
+	}
 	MPI_Finalize();
+	return 0;
 }
